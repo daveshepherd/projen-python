0ef069f3767d4f170cd5340b4b52ef2a3736b387
Dave Shepherd <dave.shepherd@unibuddy.com>
2024-07-02 17:04:20 +0100

    feat: use local projen-jsii distribution

--- .projenrc.py
+++ .projenrc.py
@@ -1,11 +1,12 @@
-from projen.python import PythonProject
+from projen_jsii import PythonPackage
 
-project = PythonProject(
-    author_email="dave.shepherd@endor.me.uk",
+project = PythonPackage(
+    author_email="dave.shepherd@unibuddy.com",
     author_name="Dave Shepherd",
     module_name="projen_python",
     name="projen-python",
     version="0.1.0",
+    deps=["../projen-jsii/dist/python/projen_jsii-0.0.0.tar.gz"],
 )
 
 project.synth()
