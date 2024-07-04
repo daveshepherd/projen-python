46b1b5c36a85057087f198258741ca1c60ce9f86
Dave Shepherd <dave.shepherd@unibuddy.com>
2024-07-04 14:24:30 +0100

    chore: update projen to introduce codeowners

--- .projenrc.py
+++ .projenrc.py
@@ -3,10 +3,11 @@ from projen_jsii import PythonPackage
 project = PythonPackage(
     author_email="dave.shepherd@unibuddy.com",
     author_name="Dave Shepherd",
+    code_owners=['sabre'],
+    deps=["../projen-jsii/dist/python/projen_jsii-0.0.0.tar.gz"],
     module_name="projen_python",
     name="projen-python",
     version="0.1.0",
-    deps=["../projen-jsii/dist/python/projen_jsii-0.0.0.tar.gz"],
 )
 
 project.synth()
